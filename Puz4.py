766fb12162a7f84efc3edcb0384924b00b545a78
Madis Jullinen <m.jullinen@gmail.com>
2014-03-23 23:36:08 +0200

    Some solutions

--- Puz4.py
+++ Puz4.py
@@ -1,1 +1,25 @@
 __author__ = 'madis'
+
+import requests
+
+
+
+a = True
+
+
+page = requests.get('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345')
+
+while a == True:
+
+
+
+    if "nothing is" in page.text:
+            number = list(page.text.split( ))[5]
+            print(page.text)
+            url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s' % number
+            page = requests.get(url)
+    else:
+        print(page.text)
+        a = False
+
+# peak.html
