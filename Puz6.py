2f307441e11d628a78199a91da3584e1f28c6769
Madis Jullinen <m.jullinen@gmail.com>
2014-06-24 07:42:32 +0300

    6 lahendatud

--- Puz6.py
+++ Puz6.py
@@ -1,6 +1,28 @@
 import requests
 import zipfile
+import os
 
-url = 'http://www.pythonchallenge.com/pc/def/channel.zip'
+# url = 'http://www.pythonchallenge.com/pc/def/channel.zip'
+#
+# r = requests.get(url)
+#
+# with open("channel.zip", "wb") as channel:
+#     channel.write(r.content)
+#
+# with zipfile.ZipFile('channel.zip', 'r') as pakk:
+#     pakk.extractall('zip')
 
-r = requests.get(url)
+loop = True
+a = '90052'
+
+while loop:
+    with open('zip/%s.txt' % a, 'r') as textfile:
+        for line in textfile:
+            a = line.split(' ')[-1]
+            print(line)
+            zf = zipfile.ZipFile('channel.zip', 'r')
+            for info in zf.infolist():  # FIXME this is stupid, think a better way of doing this...
+                if info.filename == '%s.txt' % a:
+                    with open('zip/com.txt', 'a') as kirjutis:
+                        kirjutis.write(chr(info.comment[-1]))
+                        print(chr(info.comment[-1]))
