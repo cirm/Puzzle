d1fe06f0b0c42b350d6916f9c653a75ee5c59110
Madis Jullinen <m.jullinen@gmail.com>
2014-06-24 06:34:41 +0300

    5 lahendatud

--- Puz5.py
+++ Puz5.py
@@ -1,1 +1,8 @@
-__author__ = 'madis'
+import pickle
+
+file = pickle.load(open("banner.p", "rb"))
+
+x = (len(file))
+
+for item in file:
+    print(''.join(item[z][0]*item[z][1] for z in range(len(item))))
---
+++ Puz6.py
@@ -0,0 +1,6 @@
+import requests
+import zipfile
+
+url = 'http://www.pythonchallenge.com/pc/def/channel.zip'
+
+r = requests.get(url)
